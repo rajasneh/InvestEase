55e4905ad0abce313f601db1b092bd6d40be970d
Asneh Raj <rajasneh91@gmail.com>
2025-01-30 19:46:59 +0530

    Home section done

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React from "react";
 
 function Stats() {
-    return ( <h1>Stats</h1>);
+  return (
+    <div className="container p-3">
+      <div className="row p-5">
+        <div className="col-6 p-5">
+          <h1 className="fs-2 mb-5">Trust with confidence</h1>
+          <h2 className="fs-4">Customer-first always</h2>
+          <p className="text-muted">
+            That's why 1.3+ crore customers trust InvestEase with ₹3.5+ lakh
+            crores worth of equity investments.
+          </p>
+          <h2 className="fs-4">No spam or gimmicks</h2>
+          <p className="text-muted">
+            No gimmicks, spam, "gamification", or annoying push notification.
+            High quality apps that you use at your pace, the way you like.
+          </p>
+          <h2 className="fs-4">The InvestEase universe</h2>
+          <p className="text-muted">
+            Not just an app, but a whole ecosystem. Our investments in 30+
+            fintech startups offer you tailored services specfic to your needs.
+          </p>
+          <h2 className="fs-4">Do better with money</h2>
+          <p className="text-muted">
+            With initiavies like Nudge and kill Switch, we don't just facilitate
+            transactions, but actively hlp you do better with your money.
+          </p>
+        </div>
+        <div className="col-6 p-5">
+          <img
+            src="media/images/ecosystem.png"
+            alt="ecosystem"
+            style={{ width: "93%" }}
+          ></img>
+          <div className="text-center">
+            <a
+              className="mx-5"
+              href="https://www.google.com/"
+              style={{ textDecoration: "none" }}
+            >
+              Explore our products
+              <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+            <a
+              href="https://www.google.com/"
+              style={{ textDecoration: "none" }}
+            >
+              Try kite demo
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
