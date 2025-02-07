2d02caf9fb65fe701dd37603c816866a332d917f
Asneh Raj <rajasneh91@gmail.com>
2025-02-07 22:32:37 +0530

    Fetching data from dashboard

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,34 @@
 require("dotenv").config();
 
 const express = require("express");
+const app = express();
+
 const mongoose = require("mongoose");
+const bodyParser=require("body-parser")
+const cors=require("cors");;
+
+const { PositionsModel } = require("./model/PositionsModel");
+const { HoldingsModel } = require("./model/HoldingsModel");
 
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
 
-const app = express();
-console.log(uri);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get("/allHoldings",async (req,res)=>{
+  let allHoldings=await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+app.get("/allPositions",async (req,res)=>{
+  let allPositions=await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
   // Connect to the MongoDB database
